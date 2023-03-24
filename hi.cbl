42295b1d0400d720e069783a7f2cea9741e8052a
Tom <tom@contextfree.info>
2023-03-24 11:23:55 -0700

    Explore

--- hi.cbl
+++ hi.cbl
@@ -1,5 +1,23 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. HelloWorld.
-       PROCEDURE DIVISION.
-           DISPLAY "Hello World!".
-           call "greet" using by value 5.
+       identification division.
+       program-id. hi.
+       data division.
+       working-storage section.
+       01 num pic 9(3) value 5.
+       01 txt pic x(3) value "blah".
+       procedure division.
+           display "Hello World!"
+           call "greet" using by value num
+           move 4 to num
+           call "greeet" using num
+           call "greeet" using txt.
+       end program hi.
+      *
+       identification division.
+       program-id. greeet.
+       data division.
+       linkage section.
+       01 num pic 9(3).
+       procedure division using num.
+           display "... " num " ...".
+       end program greeet.
+       
