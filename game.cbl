82750d9397711a055d936f17d9fe0f3fbc52bdc4
Tom <tom@contextfree.info>
2023-03-25 16:01:00 -0700

    Clean more

--- game.cbl
+++ game.cbl
@@ -3,7 +3,8 @@
        data division.
        working-storage section.
        copy sdl.
-       01 done pic x value space.
+       01 done-flag pic x value space.
+           88 done value 'Y'.
        01 event-found binary-long value 0.
        01 renderer usage pointer.
        01 win usage pointer.
@@ -13,13 +14,12 @@
        01 win-size binary-long value 400.
        procedure division.
            perform init
-           perform until done = 'Y'
-               perform poll-event
-               call 'SDL_RenderClear' using by value renderer
-               call 'SDL_RenderPresent' using by value renderer
-               call 'SDL_Delay' using by value 20
+           perform until done
+               perform poll-events
+               perform end-step
            end-perform
            perform dispose
+           goback
            .
        init.
            call 'SDL_Init' using by value sdl-init-video
@@ -34,18 +34,26 @@
                returning renderer
            .
        poll-event.
-           perform with test after until event-found = 0
-               call 'SDL_PollEvent' using by reference sdl-event
-                   returning event-found
+           call 'SDL_PollEvent' using by reference sdl-event
+               returning event-found
+           .
+       poll-events.
+           perform poll-event
+           perform until event-found = 0
                evaluate true
-                   when sdl-event-type-quit move 'Y' to done
+                   when sdl-event-type-quit set done to true
                end-evaluate
+               perform poll-event
            end-perform
+           .
+       end-step.
+           call 'SDL_RenderClear' using by value renderer
+           call 'SDL_RenderPresent' using by value renderer
+           call 'SDL_Delay' using by value 20
            .
        dispose.
            call 'SDL_DestroyRenderer' using by value renderer
            call 'SDL_DestroyWindow' using by value win
            call 'SDL_Quit'
-           goback
            .
        end program game.
