4db8389289028f8ebe94c53f448199d00569119b
Tom <tom@contextfree.info>
2023-03-25 20:41:20 -0700

    Try to load images

--- game.cbl
+++ game.cbl
@@ -6,12 +6,13 @@
            01 done-flag pic x value space.
                88 done value 'Y'.
            01 event-found binary-long value 0.
+           01 player-texture usage pointer.
            01 renderer usage pointer.
            01 win usage pointer.
            01 win-flags binary-long value 0.
            01 win-pos binary-long value 100.
            01 win-name pic x(32) value z'My Game'.
-           01 win-size binary-long value 400.
+           01 win-size binary-long value 700.
        procedure division.
            perform init
            perform until done
@@ -25,13 +26,18 @@
            call 'SDL_Init' using by value sdl-init-video
            call 'SDL_CreateWindow' using
               by content win-name
-              by value win-pos win-pos win-size win-size win-flags
+              by value win-pos 0 win-size 980 win-flags
               returning win
            call 'SDL_CreateRenderer' using
                by value win
+      *        TODO How to pass a null pointer?
                by reference null
                by value sdl-renderer-accelerated
                returning renderer
+           call 'loadBmp' using
+               by content z'assets/character.bmp'
+               by value renderer
+               by reference player-texture
            .
        poll-event.
            call 'SDL_PollEvent' using by reference sdl-event
@@ -57,3 +63,24 @@
            call 'SDL_Quit'
            .
        end program game.
+
+       identification division.
+       program-id. load-texture.
+       data division.
+       working-storage section.
+           01 path-nulled pic x(255).
+           01 surface usage pointer.
+       linkage section.
+           01 path pic x(254).
+           01 renderer usage pointer.
+           01 texture usage pointer.
+       procedure division.
+      *    move function concatenate(path, x'00') to path-nulled
+      *    call 'loadBmp' using by content path-nulled
+      *        returning surface
+      *    call 'SDL_CreateTextureFromSurface' using
+      *        by value renderer surface
+      *        returning texture
+      *    call 'SDL_DestroySurface' using by value surface
+      *    .
+       end program load-texture.
--- sdl.c
+++ sdl.c
@@ -4,4 +4,13 @@
 void dontCallMe(void) {
     // Just need anything to make sdl available.
     SDL_Init(0);
+    SDL_Surface* surface = SDL_LoadBMP("whatever");
+    SDL_FreeSurface(surface);
+}
+
+SDL_Texture* loadBmp(const char* path, SDL_Renderer* renderer) {
+    SDL_Surface* surface = SDL_LoadBMP(path);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    return texture;
 }
