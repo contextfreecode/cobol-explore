1480c4627bbd909f3f0674bf1c79ba76c20ebf99
Tom <tom@contextfree.info>
2023-03-30 03:36:45 -0700

    Clean hi

--- hi.cbl
+++ hi.cbl
@@ -3,22 +3,21 @@
        data division.
        working-storage section.
        01 num pic 9(3) value 5.
-       01 txt pic n(3) value x"C3A4".
+       01 txt pic x(3) value x"C3A4".
        procedure division.
            display "Hello World!"
            call "greet" using by value num
            move 4 to num
-           call "greeet" using num
-           call "greeet" using txt.
+           call "report" using num
+           call "report" using txt.
        end program hi.
-      *
+
        identification division.
-       program-id. greeet.
-      *program-id. greeet is recursive.
+       program-id. report.
+      *program-id. report is recursive.
        data division.
        linkage section.
        01 num pic 9(3).
        procedure division using num.
            display "... " num " ...".
-       end program greeet.
-       
+       end program report.
